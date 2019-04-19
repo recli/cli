eb65e93f6ff9360da098f8c19cafd60ef2df80dd
Artem Alchangyan <alchangyan@mail.ru>
2019-04-19 16:10:26 +0400

    Working handbook creator.

--- lib/local-generators/update-handbook/index.ts
+++ lib/local-generators/update-handbook/index.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import fs from 'fs';
+import { isModuleSpecifier } from '@babel/types';
 const filesToSearch = [
   'decoration',
   'hooks',
 ];
 let helpersData = [];
-
+const handbookLocation = path.join(__dirname, '../create-handbook/handbook.js');
 filesToSearch.forEach(filename => {
   const fileAsString = fs.readFileSync(path.join(__dirname, `../../${filename}.ts`), 'utf-8');
   const getRequiredDataRegExp = new RegExp(/\/\/ @(.*)\n?( *)?(export)?( )?(const) ([A-z]{0,}) = \(([A-z: ,\(\)=>]{0,})\)/g);
@@ -19,6 +20,7 @@ filesToSearch.forEach(filename => {
     const params = paramsStr.split(',');
 
     return {
+      parent: filename,
       title,
       name,
       params
@@ -28,7 +30,30 @@ filesToSearch.forEach(filename => {
   helpersData = helpersData.concat(methodsData);
 })
 
-console.log(helpersData)
+let header = '';
+const modulePaths = {};
+let handbookContent = '';
+
+helpersData.forEach(method => {
+  if (!modulePaths[method.parent]) modulePaths[method.parent] = [];
+  modulePaths[method.parent].push(method.name);
+  return handbookContent += `
+// ${method.title}
+${method.name} (${method.params}) {}
+/* ======================== */
+
+`
+}, handbookContent)
+
+if (Object.keys(modulePaths).length) {
+  for(let parent in modulePaths) {
+    header += `import {${modulePaths[parent].join(', ')}} from '${parent}';\n`
+  }
+};
+
+handbookContent = header + handbookContent;
+fs.writeFileSync(handbookLocation, handbookContent)
+// console.log(helpersData)
 
 
 // make templates for reading
